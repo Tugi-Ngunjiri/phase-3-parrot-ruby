16a8f23a893cee025d382428d488322fdf183505
tugi <blueqwinn@gmail.com>
2023-02-10 19:14:29 +0300

    first commit

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def method(parrot)
+returns(:parrot) do |sqwak|
+    puts "Sqwak #{sqwak}"
+ 
+    parrot = parrot.new
+    parrot.new = " Parrot Sqwaks!"
+# => "Parrot Sqwaks!"
+end
